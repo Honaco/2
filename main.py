63ee4a1325e1c89393da79239bd525fe8e987631
Sony <fomichevacv12@gmail.com>
2024-11-27 00:34:57 +0300

    first commit

--- main.py
+++ main.py
@@ -1,64 +1,46 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLCDNumber, QLineEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QCheckBox, QLineEdit, QPushButton
 
 
-class Exe(QWidget):
+class Ff(QWidget):
     def __init__(self):
         super().__init__()
+        self._count_widget = 4
         self.inirUI()
 
     def inirUI(self):
-        self.setGeometry(300, 300, 300, 200)
-        self.setWindowTitle('6 program')
+        self.setGeometry(300, 300, 500, 300)
+        self.setWindowTitle('Прятки для виджетов')
 
-        self.first_value = QLineEdit(self)
-        self.first_value.resize(90, 30)
-        self.first_value.move(20, 30)
+        for i in range(self._count_widget):
+            checkbox = f'checkbox{i + 1}'
+            edit = f'edit{i + 1}'
 
-        self.second_value = QLineEdit(self)
-        self.second_value.resize(90, 30)
-        self.second_value.move(20, 90)
+            setattr(self, checkbox, QCheckBox(edit, self))
+            my_check: QCheckBox = getattr(self, checkbox)
+            my_check.setChecked(True)
+            my_check.move(5, 10 + 40 * i)
+            my_check.clicked.connect(self.hide_edit)
 
-        self.b = QPushButton(self)
-        self.b.resize(30, 90)
-        self.b.move(120, 30)
-        self.b.clicked.connect(self.habr)
+            setattr(self, edit, QLineEdit(f'Поле edit{i}', self))
+            my_edit: QLineEdit = getattr(self, edit)
+            my_edit.resize(200, 30)
+            my_edit.move(150, 5 + 40 * i)
 
-        self.win1 = QLCDNumber(self)
-        self.win1.move(160, 30)
+    def hide_edit(self):
+        fiel: QCheckBox = self.sender()
+        edit: QLineEdit = getattr(self, fiel.text())
+        if fiel.isChecked():
+            edit.show()
+        else:
+            edit.hide()
 
-        self.win2 = QLCDNumber(self)
-        self.win2.move(160, 60)
-
-        self.win3 = QLCDNumber(self)
-        self.win3.move(160, 90)
-
-        self.win4 = QLCDNumber(self)
-        self.win4.move(160, 120)
-
-        self.res = 0
-
-    def habr(self):
-        n1 = int(self.first_value.text())
-        n2 = int(self.second_value.text())
-
-        self.res = n1 + n2
-        self.win1.display(self.res)
-
-        self.res = n1 - n2
-        self.win2.display(self.res)
-
-        self.res = n1 / n2
-        self.win3.display(self.res)
-
-        self.res = n1 * n2
-        self.win4.display(self.res)
 
 
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Exe()
+    ex = Ff()
     ex.show()
     exit(app.exec())
